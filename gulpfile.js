cc9fdcde54a8fc651228e12b9bcf08b9887b6672
Patrick Wenzel <patrickwenzel@Patricks-MacBook-Pro.local>
2024-06-19 23:45:37 -0400

    update packages and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var data = require('gulp-data');
-var nunjucksRender = require('gulp-nunjucks-render');
-var browserSync = require('browser-sync');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-var autoprefixer = require('gulp-autoprefixer');
-var del = require('del');
-var runSequence = require('run-sequence');
-var htmlbeautify = require('gulp-html-beautify');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const data = require('gulp-data');
+const nunjucksRender = require('gulp-nunjucks-render');
+const browserSync = require('browser-sync').create();
+const useref = require('gulp-useref');
+const uglify = require('gulp-uglify-es').default;
+const rename = require('gulp-rename');
+const gulpIf = require('gulp-if');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const del = require('del');
+const runSequence = require('run-sequence');
 
-// nunjucks
-gulp.task('nunjucks', function() {
+// Nunjucks
+function nuns() {
   return gulp.src('dev/pages/**/*.+(html|njk|nunjucks)')
     .pipe(data(function() {
       return require('./dev/data.json')
@@ -23,71 +24,58 @@ gulp.task('nunjucks', function() {
       envOptions: {autoescape: false}
     }))
     .pipe(gulp.dest('dev'))
-});
+}
 
-// Sass task
-gulp.task('sass', function() {
+// Sass
+function sassy() {
   return gulp.src('dev/scss/**/*.+(scss|sass)')
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([ require('cssnano'), require('autoprefixer')]))
     .pipe(gulp.dest('dev/css'))
     .pipe(browserSync.stream())
-});
+}
 
-// Watch task
-gulp.task('watch', ['browserSync', 'sass', 'nunjucks'], function() {
-  gulp.watch('dev/scss/**/*.+(scss|sass)', ['sass']);
-  gulp.watch('dev/pages/**/*.+(html|njk|nunjucks)', ['nunjucks']);
-  gulp.watch('dev/templates/**/*.+(html|njk|nunjucks)', ['nunjucks']);
-  gulp.watch('dev/js/**/*.js', browserSync.reload);
-  gulp.watch('dev/*.html', browserSync.reload);
-});
-
-// browserSync task
-gulp.task('browserSync', function() {
+// watch this!
+function watch() {
   browserSync.init({
     server: {
       baseDir: 'dev'
     },
-  })
-});
+  });
+  gulp.watch('dev/scss/**/*.+(scss|sass)', sassy);
+  gulp.watch('dev/pages/**/*.+(html|njk|nunjucks)', nuns);
+  gulp.watch('dev/templates/**/*.+(html|njk|nunjucks)', nuns);
+  gulp.watch('dev/js/**/*.js').on('change', browserSync.reload);
+  gulp.watch('dev/*.html').on('change', browserSync.reload);
+}
 
-// concat and minify js/css
-gulp.task('useref', function() {
+// minify ES6 because uglify is dumb
+function uggo() {
+  return gulp.src("dev/js/**/*.js")
+    .pipe(rename("main.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
+}
+
+function refs() {
   return gulp.src('dev/*.html')
     .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('build'))
-});
+}
 
-// clean up html
-gulp.task('htmlbeautify', function() {
-  gulp.src('dev/*.html')
-    .pipe(htmlbeautify({
-      indentSize: 2
-    }))
-    .pipe(gulp.dest('build'))
-});
+// add images
+function images() {
+  return gulp.src('dev/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(gulp.dest('build/images'))
+}
 
 // clean up build
-gulp.task('clean:build', function() {
-  return del.sync('build');
-});
+async function clean() {
+  del.sync('build');
+}
 
-// deletes and re-build build folder
-gulp.task('build', function (callback) {
-  runSequence(['clean:build','useref'],
-    callback
-  )
-});
+// deletes then create build folder
+gulp.task('build', gulp.series(clean, gulp.parallel(uggo,refs,images)));
 
-// starts default watch and sync task
-gulp.task('default', function (callback) {
-  runSequence(['nunjucks','sass','browserSync','watch'],
-    callback
-  )
-});
+// default tasks
+gulp.task('default', gulp.series(nuns,sassy,watch));
